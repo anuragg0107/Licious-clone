90c8c38fe67b344c3b3616017e0b7bf07b715f05
anurag gupta <101393446+anuragg0107@users.noreply.github.com>
2022-12-15 13:16:18 +0530

    push

---
+++ licious-clone/src/components/Navbar/Cart_Drawer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart_Drawer = () => {
+  return (
+    <div>Cart_Drawer</div>
+  )
+}
+
+export default Cart_Drawer
---
+++ licious-clone/src/components/Routes/PrivateRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrivateRoutes = () => {
+  return (
+    <div>PrivateRoutes</div>
+  )
+}
+
+export default PrivateRoutes
