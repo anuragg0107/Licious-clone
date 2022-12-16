4e4d615c7004340588927a57398199647fe7069a
anurag gupta <101393446+anuragg0107@users.noreply.github.com>
2022-12-16 20:31:40 +0530

    push

--- licious-clone/src/components/Navbar/Navbar.jsx
+++ licious-clone/src/components/Navbar/Navbar.jsx
@@ -45,9 +45,11 @@ const Navbar = () => {
                 <Box className='bottom_header'>
                      <Box className='bottom_container'>
                          <Box className='bottom_header_logo'>
-                              <a href='#'>
-                                <Image src='https://www.licious.in/img/rebranding/licious-logo.svg' alt='licious_logo' />
-                              </a>
+                             
+                               <Link to='/'>
+                               <Image src='https://www.licious.in/img/rebranding/licious-logo.svg' alt='licious_logo' />
+                               </Link>
+                             
                          </Box>
                          <Box className='location'>
                               <Image src='https://www.licious.in/img/rebranding/location_icon.svg' alt='location' />
@@ -74,15 +76,33 @@ const Navbar = () => {
                                 <MenuList className='scroll_data_head'  display={"inline-grid"} 
                                overflow='hidden'   borderRadius={"8px"} padding='20px' 
                                 backgroundColor={"#fff"} boxShadow="0 2px 12px 0 rgb(0 0 0 / 16% ) " marginLeft={"-100px"} marginTop="14px">
-                                 <a className='scroll_data' href="#"><Image src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/d52759ea-ba5a-0f5b-3dc1-d28875335a3f/original/Todays_Deal_1.png' alt='todays deals' height={"36px"} width="36px" margin={"10px 14px 6px 10px"}  /> <MenuItem color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"} background={"#fff"}>Today's Deals</MenuItem></a>
-                                 <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/ad04872e-60af-387c-533c-efa7dc6eb565/original/Chicken_(1).png' alt='chicken' className='categories_img' /> <MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Chicken</MenuItem></a>
-                                 <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/eb162d28-5f88-a381-1d45-7f2f66d2c776/original/FIsh.png' alt='fish' /> <MenuItem background={"#fff"}>Fish & Seafood</MenuItem></a>
-                                  <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/7113e792-7025-9e6f-6057-7174edf30856/original/Mu.png' alt='mutton' /> <MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Mutton</MenuItem></a>
-                                  <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/43faaebc-f2ae-102a-ea01-af3e6c37a890/original/RTC.png' alt='ready to cook' /> <MenuItem background={"#fff"}color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Ready to Cook</MenuItem></a>
-                                  <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/404e48e5-c887-1e12-7a3a-232e01fc9771/original/Prawn.png' alt='prawns' /><MenuItem color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"} background={"#fff"}>Prawns</MenuItem></a>
-                                  <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/5c7f1b3a-47b4-3ade-f170-f003cace2482/original/Coldcuts.png' alt='cold cuts'/><MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Cold Cuts</MenuItem></a>
-                                  <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/88d8c760-ee1f-7696-7827-fcc9661925cc/original/Spread_Bottle1.png' alt='spread' /><MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Spread</MenuItem></a>
-                                  <a className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/04d58840-f014-e613-24d4-1908db9a7e63/original/Eggs.png' alt='eggs' /><MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Eggs</MenuItem></a>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data' ><Image src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/d52759ea-ba5a-0f5b-3dc1-d28875335a3f/original/Todays_Deal_1.png' alt='todays deals' height={"36px"} width="36px" margin={"10px 14px 6px 10px"}  /> <MenuItem color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"} background={"#fff"}>Today's Deals</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/ad04872e-60af-387c-533c-efa7dc6eb565/original/Chicken_(1).png' alt='chicken' className='categories_img' /> <MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Chicken</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/eb162d28-5f88-a381-1d45-7f2f66d2c776/original/FIsh.png' alt='fish' /> <MenuItem background={"#fff"}>Fish & Seafood</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/7113e792-7025-9e6f-6057-7174edf30856/original/Mu.png' alt='mutton' /> <MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Mutton</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/43faaebc-f2ae-102a-ea01-af3e6c37a890/original/RTC.png' alt='ready to cook' /> <MenuItem background={"#fff"}color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Ready to Cook</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/404e48e5-c887-1e12-7a3a-232e01fc9771/original/Prawn.png' alt='prawns' /><MenuItem color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"} background={"#fff"}>Prawns</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/5c7f1b3a-47b4-3ade-f170-f003cace2482/original/Coldcuts.png' alt='cold cuts'/><MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Cold Cuts</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/88d8c760-ee1f-7696-7827-fcc9661925cc/original/Spread_Bottle1.png' alt='spread' /><MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Spread</MenuItem></Text>
+                                </Link>
+                                <Link to='/chicken'>
+                                <Text className='scroll_data'><Image height={"36px"} width="36px"  margin={"10px 14px 6px 10px"} src='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/04d58840-f014-e613-24d4-1908db9a7e63/original/Eggs.png' alt='eggs' /><MenuItem background={"#fff"} color="#4b4f54" fontWeight={"500"} lineHeight="19px" fontSize={"16px"}>Eggs</MenuItem></Text>
+                                </Link>
                                 </MenuList>
                               </Portal>
                            </Menu>
@@ -111,7 +131,8 @@ const Navbar = () => {
                             </Box>
                             <Box className='cart_section'>
                               <a className='cart_data'>
-                              <Image color={"#000"} cursor="pointer" src='https://www.licious.in/img/rebranding/cart_icon.svg' alt='cart' 
+                              <Image color={"#000"} cursor="pointer"
+                               src='https://www.licious.in/img/rebranding/cart_icon.svg' alt='cart' 
                                 onClick={() => drawerBag.onOpen()}/>
                               </a>
                             </Box>
@@ -123,7 +144,7 @@ const Navbar = () => {
                 </Box>
                 </Box>
                 <Bag_Drawer onClose={drawerBag.onClose} isOpen={drawerBag.isOpen}/>
-                {/* <Login onClose={drawerBag.onClose} isOpen={drawerBag.isOpen} />      */}
+                
     </>
   )
 }
