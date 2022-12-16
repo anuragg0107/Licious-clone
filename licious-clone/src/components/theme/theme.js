d14d5931963263bbcb5e71dadf32a1bf1e401358
meenakshi singh <meenakshisinghthakur4668@gmail.com>
2022-12-16 14:17:07 +0530

    login

---
+++ licious-clone/src/components/theme/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#8897a2",
+      // ...
+      900: "#1a202c",
+      500: "red",
+    },
+  },
+});
