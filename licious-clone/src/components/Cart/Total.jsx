c6f02bcc4539970db1f2b97a28432480e67af86a
Harshank11 <hrsk321@gmail.com>
2022-12-17 11:22:16 +0530

    day5

---
+++ licious-clone/src/components/Cart/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({ total }) => {
+  // console.log(total)
+    return (
+      <div style={{  color:'red',fontWeight:'bold',fontSize:"18px",marginLeft:'5px'}}>
+         {total} 
+      </div>
+    );
+  };
+  export default Total;
