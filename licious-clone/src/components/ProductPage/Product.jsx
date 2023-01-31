451c3142cfb223c6954b1af234634ca4fe70a1fd
Harshank11 <hrsk321@gmail.com>
2023-01-31 12:08:18 +0530

    Responsiveness for Products page

--- licious-clone/src/components/ProductPage/Product.jsx
+++ licious-clone/src/components/ProductPage/Product.jsx
@@ -13,12 +13,13 @@ const Product = ({item}) => {
   return (
     <div>
       <div className="product_first_div1" key={id}>
-      <Link to={`/chicken/${id}`}><img className="Imageabcd" src={image} alt="abcd" />
+      <Link to={`/chicken/${id}`}><img className="ProductImage" src={image} alt="abcd" />
         <h3 className="product_title1">{title}</h3>
         <p className="product_description_1">{description}</p>
         <div className="product_weight1">
           <p className="product_weight2">{wet}</p>
-        </div></Link>
+        </div>
+        </Link>
         <div className="product_weight1">
           <h3 className="product_price1"> MRP:₹{price}</h3>
           <button className="product_add_cart1" onClick={addToCartFunction}>Add To Cart</button>
